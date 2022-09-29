ad793f7c85ebfcd44a4f1d62f9eeeb30f11f5235
codethi <docencia.thiago@gmail.com>
2022-09-29 17:02:11 -0300

    refact: test props styled components

--- src/components/ProductRow.js
+++ src/components/ProductRow.js
@@ -1,16 +1,22 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 export default function ProductRow(props) {
   const { name, price, stocked } = props.product;
+  const [shine, setShine] = useState(false);
+
   return (
-    <ProductRowContainer stocked={stocked}>
+    <ProductRowContainer
+      stocked={stocked}
+      shine={shine}
+      onClick={() => setShine(!shine)}
+    >
+      <button>Delete</button>
       <p>{name}</p>
       <p>{price}</p>
     </ProductRowContainer>
   );
 }
-
-/* {`product-row ${!stocked ? "not-in-stock" : ""}`} */
 
 const ProductRowContainer = styled.div`
   display: flex;
@@ -19,7 +25,18 @@ const ProductRowContainer = styled.div`
   padding: 10px;
   margin: 10px;
   font-weight: 700;
+  cursor: pointer;
 
   color: ${({ stocked }) => (stocked ? "black" : "red")};
   background-color: ${({ stocked }) => (stocked ? "#ccc" : "#ffcccc")};
+
+  transform: ${({ shine }) => (shine ? "scale(1.1)" : "")};
+
+  button {
+    display: none;
+  }
+
+  :hover button {
+    display: block;
+  }
 `;
